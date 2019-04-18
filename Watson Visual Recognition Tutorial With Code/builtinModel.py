1d807807e18d8077fd9d36be8478992542d3099c
DraganKov <dragsterd@yahoo.ca>
2019-04-18 02:39:05 -0400

    edit apii key

--- Watson Visual Recognition Tutorial With Code/builtinModel.py
+++ Watson Visual Recognition Tutorial With Code/builtinModel.py
@@ -2,6 +2,6 @@ import json
 import fileinput
 from watson_developer_cloud import VisualRecognitionV3, WatsonApiException
 
-visual_recognition = VisualRecognitionV3('2018-03-19',iam_apikey='09tx6UjaBn4dvOecmCRTGQe4e6uoXDHGow7_mFvqOr4N')
+visual_recognition = VisualRecognitionV3('2018-03-19',iam_apikey='0jaBn4dvOecmCRTGQe4e6uoXDHGow7_mFvqOr4N')
 response = visual_recognition.get_classifier('fire_types_1841952908').get_result()
 print(response)
